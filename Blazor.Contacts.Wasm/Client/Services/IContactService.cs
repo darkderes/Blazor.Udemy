0cede30f2539c348b72efcf02c2b3c4a5e513de7
Jorge Darderes <jdarderes@gmail.com>
2021-07-14 12:39:34 -0400

    Add project files.

---
+++ Blazor.Contacts.Wasm/Client/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using Blazor.Contacts.Wasm.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Contacts.Wasm.Client.Services
+{
+    public interface IContactService
+    {
+        Task SaveContact (Contact contact);
+        Task DeleteContact(int id);
+       // Task UpdateContact(Contact contact);
+        Task<IEnumerable<Contact>> GetAll();
+        Task<Contact> GetDetails( int i);
+    }
+}
